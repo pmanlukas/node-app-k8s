c0112027bb92f569619240bf3a0f2487bde99bb1
Lukas Pman <lukas.pollmann@tum.de>
2022-11-07 14:59:45 +0000

    update init repo

---
+++ bootstrap/init.sh
@@ -0,0 +1,17 @@
+# Initilize set project ID variable and run various initializations
+# ACTION REQUIRED! Change "project-id-here" value to the project you'll be using
+#test
+export PROJECT_ID="dunder-mifflin-363209"
+# sets the current project for gcloud
+gcloud config set project $PROJECT_ID
+# Enables various APIs you'll need
+gcloud services enable container.googleapis.com cloudbuild.googleapis.com \
+artifactregistry.googleapis.com clouddeploy.googleapis.com \
+cloudresourcemanager.googleapis.com
+# creates the Artifact Registry repo
+gcloud artifacts repositories create node-app --location=us-central1 \
+--repository-format=docker
+# creates the Google Cloud Deploy pipeline
+gcloud beta deploy apply --file clouddeploy.yaml \
+--region=us-central1 --project=$PROJECT_ID
+echo "init done. To create clusters, run: ./gke-cluster-init.sh"
