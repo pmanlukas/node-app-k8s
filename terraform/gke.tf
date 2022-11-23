4a3618013c35beb2673be48e5bca64563c0e1aa2
Lukas Pman <lukas.pollmann@tum.de>
2022-11-23 10:59:14 +0000

    update config and add vars file

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,16 +1,37 @@
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
 }
  
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
- 
-# GKE cluster
+# GKE clusters
+
+#testcluster
 resource "google_container_cluster" "primary" {
-  name     = "${var.project_id}-gke"
+  name     = "testcluster"
+  location = var.region
+ 
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+ 
+# Enabling Autopilot for this cluster
+  enable_autopilot = true
+}
+
+#stagingcluster
+resource "google_container_cluster" "secondary" {
+  name     = "stagingcluster"
+  location = var.region
+ 
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+ 
+# Enabling Autopilot for this cluster
+  enable_autopilot = true
+}
+
+#prodcluster
+resource "google_container_cluster" "third" {
+  name     = "prodcluster"
   location = var.region
  
   network    = google_compute_network.vpc.name
